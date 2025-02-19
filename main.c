da4c4437fafbd7189bc1df0cf047feb384ea11d2
Ubuntu <ubuntu@ip-172-31-10-151.ap-south-1.compute.internal>
2025-02-19 16:40:34 +0000

    add .c filrs to RR

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Function prototypes
+void biggest();
+void factorial();
+void paliendrome();
+
+int main() {  // Explicit return type added
+    biggest();
+    factorial();
+    paliendrome();
+    return 0; // Proper return statement
+}
+
